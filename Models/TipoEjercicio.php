21e48ac83b07c9f9c12d17ee327cd9293d1940ef
Hilanai <hilagoncruz@gmail.com>
2026-02-25 20:09:34 -0600

    finalcommit
    commit

--- Models/TipoEjercicio.php
+++
@@ -1,9 +0,0 @@
-<?php // app/Models/TipoEjercicio.php
-require_once __DIR__ . '/DB.php';
-
-class TipoEjercicio {
-  public static function all(): array {
-    $sql = "SELECT TipoEjercicio, DescripcionTipoEjercicio FROM tiposejercicio ORDER BY 1";
-    return DB::conn()->query($sql)->fetchAll();
-  }
-}
