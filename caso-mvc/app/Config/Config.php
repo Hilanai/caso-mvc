db4e30102eb4e151a4c98f6d34df0ab9b2bd60e4
Hilanai <hilagoncruz@gmail.com>
2026-02-25 21:13:24 -0600

    Conexión de base de datos finalizada

--- caso-mvc/app/Config/Config.php
+++ caso-mvc/app/Config/Config.php
@@ -9,5 +9,5 @@ define('DB_USER', 'root');
 define('DB_PASS', ''); // si tu MySQL tiene clave, cámbiala aquí
 
 // Ruta base para generar URLs (ajústala si el nombre de carpeta cambia)
-define('BASE_URL', '/caso-mvc/public');
+define('BASE_URL', 'http://localhost:81/caso-mvc');
 ``
